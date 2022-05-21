517c4287545f230d3ffbed651eb36d6fcf03d47b
syillvy <101952879+syillvy@users.noreply.github.com>
2022-05-21 12:41:56 +0100

    Delete Stopwatch directory

--- Stopwatch/src/components/App/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Stopwatch } from "src/components/Stopwatch";
-
-export default function App() {
-  return <Stopwatch />;
-}
--- Stopwatch/src/lib/time.ts
+++
@@ -1,3 +0,0 @@
-export const Second = 1000;
-export const Minute = 60 * Second;
-export const Hour = 60 * Minute;
